8b4b2bde450f81b5b1c9bf3d5913f75145e8c938
cupnodes <aldairgomesdecastrosales@gmail.com>
2021-09-01 09:02:55 -0300

    Atualizações de Desing

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const newYears = '1 Jan 2022'
+const newYears = '2022-1-1'
 
 const daysElement = document.getElementById('days')
 const hoursElement = document.getElementById('hours')
 const minutesElement = document.getElementById('minutes')
 const secondsElement = document.getElementById('seconds')
 
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
 function countDown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000; // Dividir por mil pois o Date retorna milisegundos
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
+    const days = Math.floor(totalSeconds / (3600 * 24));
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds /60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
-
-    // console.log(days, hours, minutes, seconds);
 
     daysElement.innerHTML = formatTime(days);
     hoursElement.innerHTML = formatTime(hours);
     minutesElement.innerHTML = formatTime(minutes);
     secondsElement.innerHTML = formatTime(seconds);
-}
-
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
 }
 
 countDown();
